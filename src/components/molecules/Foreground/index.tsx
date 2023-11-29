9a781c43606f6795416c2d59fcb3ac8458ec7a0c
tatello <tatellog@gmail.com>
2023-11-28 22:06:18 -0600

    transitions were added

--- src/components/molecules/Foreground/index.tsx
+++ src/components/molecules/Foreground/index.tsx
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useMotionValue, useTransform } from 'framer-motion'
 
 import { CircleMedium, CircleSmall, ForegroundContainer } from './styles'
 
 const Foreground = () => {
   const scrollY = useMotionValue(0)
-  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-  const finalTopPercentage = 42
-  const finalLeftPercentage = 80
-
-  const translateY = useTransform(
-    scrollY,
-    [0, 300],
-    [0, -(finalTopPercentage * windowHeight) / 160],
-  )
-
-  const translateX = useTransform(
-    scrollY,
-    [0, 400],
-    [0, -(finalLeftPercentage * windowWidth) / 350 - 150],
-  )
-
+  const translateY = useTransform(scrollY, [0, 300], [80, -250])
+  const translateX = useTransform(scrollY, [0, 400], [0, -470])
   const scale = useTransform(scrollY, [0, 400], [1, 1.6])
 
   useEffect(() => {
@@ -30,20 +14,11 @@ const Foreground = () => {
       scrollY.set(window.scrollY)
     }
 
-    const handleResize = () => {
-      setWindowHeight(window.innerHeight)
-      setWindowWidth(window.innerWidth)
-    }
-
     window.addEventListener('scroll', updateScrollY)
-    window.addEventListener('resize', handleResize)
-
     return () => {
       window.removeEventListener('scroll', updateScrollY)
-      window.removeEventListener('resize', handleResize)
     }
-  }, [scrollY])
-
+  }, [])
   return (
     <ForegroundContainer id="foreground">
       <CircleSmall
